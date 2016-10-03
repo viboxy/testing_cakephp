e6ada1966214e370ee93cc0fc3aaf730026e3773
vagrant <vagrant@vagrant.vm>
2016-10-03 04:37:24 +0000

    Create CurrencyRenderHelper class and Test cases

--- src/View/Helper/CurrencyRendererHelper.php
+++ src/View/Helper/CurrencyRendererHelper.php
@@ -4,7 +4,7 @@ namespace App\View\Helper;
 use Cake\View\Helper;
 
 class CurrencyRendererHelper extends Helper{
-    public function usd($amount){
+    public function usd($amount = null){
         return'USD '.number_format($amount, 2, '.', ',');
     }
 }
