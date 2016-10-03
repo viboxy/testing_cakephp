701d6aab581a87b608b40ad7ec64e485ff474512
hemantha <viboxy@gmail.com>
2016-10-03 14:04:17 +0530

    Modify the ArticlesControllerTest class  test cases

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -6,7 +6,7 @@ use Cake\ORM\Table;
 use Cake\Validation\Validator;
 
 class ArticlesTable extends Table{
-
+    
     public function initialize(array $config)
     {
         $this->addBehavior('Timestamp');
@@ -27,4 +27,8 @@ class ArticlesTable extends Table{
         ]);
         return $query;
     }
+
+    /*public function findTitle($title){
+        $result = 
+    }*/
 }
